f9f70875bdacb1af8d7a50fdb35a776541eeded6
kenshi kamata <kenshi.kamata@gmail.com>
2013-04-26 00:47:12 +0900

    user setting updated
    ユーザごとの日報の出力先を設定できるようにしたが，うまくいっていない．

--- daily_report/Form1.Designer.cs
+++ daily_report/Form1.Designer.cs
@@ -28,34 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 66);
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(3, 377);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(260, 19);
+            this.textBox1.Size = new System.Drawing.Size(232, 27);
             this.textBox1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(40, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(195, 24);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "この時間なにした？";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 105);
+            this.button1.Location = new System.Drawing.Point(3, 410);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(259, 23);
+            this.button1.Size = new System.Drawing.Size(232, 26);
             this.button1.TabIndex = 2;
             this.button1.Text = "登録する";
             this.button1.UseVisualStyleBackColor = true;
@@ -63,25 +58,49 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(13, 165);
+            this.button2.Location = new System.Drawing.Point(241, 410);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(259, 61);
+            this.button2.Size = new System.Drawing.Size(209, 26);
             this.button2.TabIndex = 3;
             this.button2.Text = "出力する";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(310, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(242, 377);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(208, 27);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "出力先を変更する";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(462, 440);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,9 +109,11 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- daily_report/Form1.cs
+++ daily_report/Form1.cs
@@ -13,14 +13,20 @@ namespace daily_report
     public partial class Form1 : Form
     {
         today_report today;
+
         public Form1()
         {
             InitializeComponent();
             this.today = new today_report();
 
-        }
+            if (Properties.Settings.Default.output_dir_path == "")
+            {
+                MessageBox.Show("日報の出力先が設定されていないので，設定をお願いします");
+                this.set_output_dir_path();
+            }
 
-        private void button1_Click(object sender, EventArgs e)
+        }
+        private void button1_Click(object sender, EventArgs e)//日報のトピックを登録するボタンが押されたとき
         {
             if (this.textBox1.Text == "")//一応入力のチェックはする
             {
@@ -34,12 +40,57 @@ namespace daily_report
             this.textBox1.Text = "";
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)//出力ボタンが押されたとき
         {
-            StreamWriter w = new StreamWriter("path\\to\\directory" + DateTime.Now.ToString("yyyymmdd") + "_daily_report.txt");
+            StreamWriter w = new StreamWriter(this.get_output_dir_path() + @"\" +DateTime.Now.ToString("yyyymmdd") + "_daily_report.txt");
             this.today.report(w);
             w.Close();
             MessageBox.Show("出力しました！");
+            System.Diagnostics.Process.Start(this.get_output_dir_path());
+        }
+
+        private void button3_Click(object sender, EventArgs e)//出力先変更ボタンが押されたとき
+        {
+            this.set_output_dir_path();
+        }
+
+        private void set_output_dir_path()//出力先を設定する．
+        {
+            /*デバッグ用出力先
+            MessageBox.Show(Properties.Settings.Default.output_dir_path);
+            MessageBox.Show(System.Environment.GetEnvironmentVariable("HOME"));
+            */
+
+            // ダイアログの説明を設定する
+            folderBrowserDialog1.Description = "出力先フォルダを選んでください";
+
+            // ルートになる特殊フォルダを設定する (初期値 SpecialFolder.Desktop)
+            folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyDocuments;
+
+            // 初期選択するパスを設定する
+            folderBrowserDialog1.SelectedPath = System.Environment.GetEnvironmentVariable("HOME");
+
+            // [新しいフォルダ] ボタンを表示する (初期値 true)
+            folderBrowserDialog1.ShowNewFolderButton = true;
+
+            // ダイアログを表示し、戻り値が [OK] の場合は、選択したディレクトリを表示する
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //MessageBox.Show(folderBrowserDialog1.SelectedPath);
+                Properties.Settings.Default.output_dir_path = folderBrowserDialog1.SelectedPath;
+            }
+            else
+            {
+                MessageBox.Show("問題があったため，初期設定を利用します");
+            }
+            // 不要になった時点で破棄する (正しくは オブジェクトの破棄を保証する を参照)
+            folderBrowserDialog1.Dispose();
+            Properties.Settings.Default.Save();
+        }
+
+        private string get_output_dir_path()
+        {
+            return Properties.Settings.Default.output_dir_path;
         }
     }
 
@@ -84,7 +135,7 @@ namespace daily_report
 
         public String show_what_did()
         {
-            return this.dt.ToString("yyyy年MM月dd日 HH時mm分ss秒 :: ") + this.content + "\n";
+            return this.dt.ToString("yyyy年MM月dd日 HH時mm分ss秒 :: ") + this.content;// +"\n";
         }
     }
 }
